09266027063553906fe1637ec6218b617979c6df
Thien-Thi Nguyen <ttn@gnuvola.org>
2001-09-26 03:16:26 +0000

    (SCM_MAKE_CHAR): Use `scm_t_bits' instead of `intptr_t'. Thanks to Golubev I. N.

--- libguile/chars.h
+++ libguile/chars.h
@@ -3,17 +3,17 @@
 #ifndef SCM_CHARSH
 #define SCM_CHARSH
 /*	Copyright (C) 1995,1996, 2000, 2001 Free Software Foundation, Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -51,7 +51,7 @@
  */
 #define SCM_CHARP(x) (SCM_ITAG8(x) == scm_tc8_char)
 #define SCM_CHAR(x) ((unsigned int)SCM_ITAG8_DATA(x))
-#define SCM_MAKE_CHAR(x) SCM_MAKE_ITAG8((intptr_t) x, scm_tc8_char)
+#define SCM_MAKE_CHAR(x) SCM_MAKE_ITAG8((scm_t_bits) x, scm_tc8_char)
 
 
 
