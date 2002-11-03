0e94a6b0a4f9f48c43b9ff4d970380fbe287387b
Marius Vollmer <mvo@zagadka.de>
2002-11-03 17:23:20 +0000

    Include "_scm.h" before testing HAVE_UNISTD_H.  Thanks to Bill Schottstaedt!

--- libguile/iselect.c
+++ libguile/iselect.c
@@ -1,4 +1,4 @@
-/*	Copyright (C) 1997, 1998, 2000, 2001 Free Software Foundation, Inc.
+/*	Copyright (C) 1997, 1998, 2000, 2001, 2002 Free Software Foundation, Inc.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,11 +44,12 @@
 #include <limits.h>
 #include <string.h>
 
+#include "libguile/_scm.h"
+
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 
-#include "libguile/_scm.h"
 #include "libguile/async.h"
 
 #include "libguile/iselect.h"
