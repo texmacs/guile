b2981fd6fd84b242c3615d584eb787eff6f8b075
Rob Browning <rlb@defaultvalue.org>
2003-09-27 04:21:59 +0000

    remove vestigal (pre-compile-psyntax.scm) load of psyntax.ss (used to be needed during syntax-case development, but now only caused problems with the top-of-the-file eval export. Enabling the code as the comments instructed caused the other error when trying to recompile psyntax.pp.

--- ice-9/syncase.scm
+++ ice-9/syncase.scm
@@ -243,10 +243,6 @@
 		  (read-options old-read))))
 
 
-;;; The following lines are necessary only if we start making changes
-;; (use-syntax sc-expand)
-;; (load-from-path "ice-9/psyntax.ss")
-
 (define internal-eval (nested-ref the-scm-module '(app modules guile eval)))
 
 (define (eval x environment)
