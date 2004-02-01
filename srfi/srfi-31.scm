819f477aba45f6891a4e15beb9c0d36c9b555e02
Rob Browning <rlb@defaultvalue.org>
2004-02-01 21:41:11 +0000

    new file.

---
+++ srfi/srfi-31.scm
@@ -0,0 +1,57 @@
+;;; srfi-34.scm --- Time/Date Library
+
+;; 	Copyright (C) 2004 Free Software Foundation, Inc.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this software; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;; Boston, MA 02111-1307 USA
+;;
+;; As a special exception, the Free Software Foundation gives permission
+;; for additional uses of the text contained in its release of GUILE.
+;;
+;; The exception is that, if you link the GUILE library with other files
+;; to produce an executable, this does not by itself cause the
+;; resulting executable to be covered by the GNU General Public License.
+;; Your use of that executable is in no way restricted on account of
+;; linking the GUILE library code into it.
+;;
+;; This exception does not however invalidate any other reasons why
+;; the executable file might be covered by the GNU General Public License.
+;;
+;; This exception applies only to the code released by the
+;; Free Software Foundation under the name GUILE.  If you copy
+;; code from other Free Software Foundation releases into a copy of
+;; GUILE, as the General Public License permits, the exception does
+;; not apply to the code that you add in this way.  To avoid misleading
+;; anyone as to the status of such modified files, you must delete
+;; this exception notice from them.
+;;
+;; If you write modifications of your own for GUILE, it is your choice
+;; whether to permit this exception to apply to your modifications.
+;; If you do not wish that, delete this exception notice.
+
+(define-module (srfi srfi-31)
+  :export-syntax (rec))
+
+(define-macro (rec arg-form . body)
+  (cond
+   ((and (symbol? arg-form) (= 1 (length body)))
+    ;; (rec S (cons 1 (delay S)))
+    `(letrec ((,arg-form ,(car body)))
+       ,arg-form))
+   ;; (rec (f x) (+ x 1))
+   ((list? arg-form)
+    `(letrec ((,(car arg-form) (lambda ,(cdr arg-form) ,@body)))
+       ,(car arg-form)))
+   (else "syntax error in rec form" `(rec ,arg-form ,@body))))
