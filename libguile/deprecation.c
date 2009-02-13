5e5b073faa66630c0f680145e57612582fd92b68
daniel <daniel@dani.localdomain.invalid>
2009-02-13 14:27:41 +0100

    Make `--disable-deprecated' work.
    * configure.in (enable_deprecated): Set SCM_WARN_DEPRECATED_DEFAULT
      even when --disable-deprecated is passed.
    * libguile/deprecation.h: Declare deprecation-issuing methods even
      if SCM_ENABLE_DEPRECATED is not set.
    * libguile/deprecation.c: Ditto.
      (scm_init_deprecation): Include full body even for unset
      SCM_ENABLE_DEPRECATED.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- libguile/deprecation.c
+++ libguile/deprecation.c
@@ -38,8 +38,6 @@
 
 
 
-#if (SCM_ENABLE_DEPRECATED == 1)
-
 struct issued_warning {
   struct issued_warning *prev;
   const char *message;
@@ -135,8 +133,6 @@ print_deprecation_summary (void)
     }
 }
 
-#endif /* SCM_ENABLE_DEPRECATED == 1 */
-
 SCM_DEFINE(scm_include_deprecated_features,
 	   "include-deprecated-features", 0, 0, 0,
 	   (),
@@ -154,7 +150,6 @@ SCM_DEFINE(scm_include_deprecated_features,
 void
 scm_init_deprecation ()
 {
-#if (SCM_ENABLE_DEPRECATED == 1)
   const char *level = getenv ("GUILE_WARN_DEPRECATED");
   if (level == NULL)
     level = SCM_WARN_DEPRECATED_DEFAULT;
@@ -167,7 +162,6 @@ scm_init_deprecation ()
       SCM_WARN_DEPRECATED = 0;
       atexit (print_deprecation_summary);
     }
-#endif
 #include "libguile/deprecation.x"
 }
 
