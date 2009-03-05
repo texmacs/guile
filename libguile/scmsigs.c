499c43b03225abb8d3af9087b7630d523b74e13a
Neil Jerram <neil@ossau.uklinux.net>
2009-03-05 20:03:33 +0000

    Avoid throw from critical section, given invalid sigaction call
    * libguile/scmsigs.c (scm_sigaction_for_thread): Exit critical section
      before raising out-of-range error.
    
    * test-suite/Makefile.am (SCM_TESTS): Add signals.test.
    
    * test-suite/tests/signals.test: New file.

--- libguile/scmsigs.c
+++ libguile/scmsigs.c
@@ -356,7 +356,10 @@ SCM_DEFINE (scm_sigaction_for_thread, "sigaction", 1, 3, 0,
 	  install_handler (csig, SCM_BOOL_F, SCM_BOOL_F, async);
 	}
       else
-	SCM_OUT_OF_RANGE (2, handler);
+	{
+	  SCM_CRITICAL_SECTION_END;
+	  SCM_OUT_OF_RANGE (2, handler);
+	}
     }
   else if (scm_is_false (handler))
     {
