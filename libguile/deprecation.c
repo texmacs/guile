de8ab91a76277f669d2225396135d25da653cd7d
Ludovic Courtès <ludo@gnu.org>
2009-07-05 21:13:02 +0200

    Work around the lack of a vsnprintf(3) declaration on AIX 5.3.
    * configure.in: Check for the vsnprintf(3) declaration.
    
    * libguile/deprecation.c [!HAVE_DECL_VSNPRINTF]: Provide vsnprintf(3)
      declaration.  This is needed on `powerpc-ibm-aix5.3.0.0'.

--- libguile/deprecation.c
+++ libguile/deprecation.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001, 2006 Free Software Foundation, Inc.
+/* Copyright (C) 2001, 2006, 2009 Free Software Foundation, Inc.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -33,7 +33,9 @@
 
 /* Windows defines. */
 #ifdef __MINGW32__
-#define vsnprintf _vsnprintf
+# define vsnprintf _vsnprintf
+#elif !HAVE_DECL_VSNPRINTF
+extern int vsnprintf (char *, size_t, const char *, va_list);
 #endif
 
 
