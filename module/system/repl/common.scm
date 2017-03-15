db19853b0668fbd76dd9fc07b451102e32e8a36d
Andy Wingo <wingo@pobox.com>
2017-03-15 09:12:55 +0100

    Update --version and REPL copyright years
    * module/ice-9/command-line.scm (version-etc):
    * module/system/repl/common.scm (*version*): Update release year.

--- module/system/repl/common.scm
+++ module/system/repl/common.scm
@@ -40,7 +40,7 @@
 
 (define *version*
   (format #f "GNU Guile ~A
-Copyright (C) 1995-2016 Free Software Foundation, Inc.
+Copyright (C) 1995-2017 Free Software Foundation, Inc.
 
 Guile comes with ABSOLUTELY NO WARRANTY; for details type `,show w'.
 This program is free software, and you are welcome to redistribute it
