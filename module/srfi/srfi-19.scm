texmacs/guile

commit b11e2922c36c4105797c269c7e616535b702698a

SRFI-19 current-time-monotonic returns time of right type
* module/srfi/srfi-19.scm (current-time-monotonic): Actually return a
  time with the correct type.  Fixes #26329.

module/srfi/srfi-19.scm
@@ -1,6 +1,6 @@
 ;;; srfi-19.scm --- Time/Date Library
 
-;; Copyright (C) 2001-2003, 2005-2011, 2014, 2016
+;; Copyright (C) 2001-2003, 2005-2011, 2014, 2016-2017
 ;;   Free Software Foundation, Inc.
 ;;
 ;; This library is free software; you can redistribute it and/or
@@ -333,8 +333,11 @@
 ;;    of course.
 
 (define (current-time-monotonic)
-  ;; Resolution is microseconds.
-  (current-time-tai))
+  ;; Guile monotonic and TAI times are the same.
+  (let ((tai (current-time-tai)))
+    (make-time time-monotonic
+               (time-second tai)
+               (time-nanosecond tai))))
 
 (define (current-time-thread)
   (time-error 'current-time 'unsupported-clock-type 'time-thread))

commit 886ac3e2abce89bd3f47f957c36bcec16613c509

SRFI-19: Swap seconds and nanoseconds in 'current-time-monotonic'.
* module/srfi/srfi-19.scm (current-time-monotonic): Swap the 2nd and 3rd
arguments.  Fixes a regression introduced in
commit b11e2922c36c4105797c269c7e616535b702698a.

module/srfi/srfi-19.scm
@@ -336,8 +336,8 @@
   ;; Guile monotonic and TAI times are the same.
   (let ((tai (current-time-tai)))
     (make-time time-monotonic
-               (time-second tai)
-               (time-nanosecond tai))))
+               (time-nanosecond tai)
+               (time-second tai))))
 
 (define (current-time-thread)
   (time-error 'current-time 'unsupported-clock-type 'time-thread))