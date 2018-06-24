texmacs/guile

commit efc33cd1497c00c5ebf961e834efc1d85f3e28ac

vm: Fix typo when checking for 'madvise' error code.
* libguile/vm.c (return_unused_stack_to_os): Check for EAGAIN, not -EAGAIN.

libguile/vm.c
@@ -903,6 +903,7 @@ return_unused_stack_to_os (struct scm_vm *vp)
       do
         ret = madvise ((void *) lo, hi - lo, MADV_DONTNEED);
       while (ret && errno == -EAGAIN);
+      while (ret && errno == EAGAIN);
 
       if (ret)
         perror ("madvise failed");

commit 444648441ad7550e8afad8c7dfaa00605d727eba

vm: Fix another typo.
Fix typo introduced in efc33cd1497c00c5ebf961e834efc1d85f3e28ac.

* libguile/vm.c (return_unused_stack_to_os): Remove extra 'while'.

libguile/vm.c
@@ -902,7 +902,6 @@ return_unused_stack_to_os (struct scm_vm *vp)
 
       do
         ret = madvise ((void *) lo, hi - lo, MADV_DONTNEED);
-      while (ret && errno == -EAGAIN);
       while (ret && errno == EAGAIN);
 
       if (ret)