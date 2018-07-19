e45a33ce5884aaeae3add4144a262161333d495b
Ludovic Courtès <ludo@gnu.org>
2018-07-19 17:58:39 +0200

    compile: Update copyright year.
    * module/scripts/compile.scm (show-version): Update copyright year.

--- module/scripts/compile.scm
+++ module/scripts/compile.scm
@@ -156,7 +156,7 @@ options."
 
 (define (show-version)
   (format #t "compile (GNU Guile) ~A~%" (version))
-  (format #t "Copyright (C) 2009, 2011 Free Software Foundation, Inc.
+  (format #t "Copyright (C) 2018 Free Software Foundation, Inc.
 License LGPLv3+: GNU LGPL version 3 or later <http://gnu.org/licenses/lgpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.~%"))
