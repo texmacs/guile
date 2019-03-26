3acf10280bbe2d1527caae1e5d1fd60208d898d1
Andy Wingo <wingo@igalia.com>
2019-03-26 10:11:12 +0100

    Add mov_addr test

---
+++ tests/mov_addr.c
@@ -0,0 +1,23 @@
+#include "test.h"
+
+static uint64_t thing = 0x123456789abcdef0;
+
+static void
+run_test(jit_state_t *j, uint8_t *arena_base, size_t arena_size)
+{
+  jit_begin(j, arena_base, arena_size);
+
+  jit_patch_there(j, jit_mov_addr(j, JIT_R0), &thing);
+  jit_retr(j, JIT_R0);
+
+  void* (*f)(void) = jit_end(j, NULL);
+
+  ASSERT(f() == &thing);
+  ASSERT(*(uint64_t*)f() == thing);
+}
+
+int
+main (int argc, char *argv[])
+{
+  return main_helper(argc, argv, run_test);
+}
