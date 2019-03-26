d917dc21478f17ccde29ee25e012f0076982ac91
Andy Wingo <wingo@igalia.com>
2019-03-26 11:24:31 +0100

    Add ldxr tests

---
+++ tests/ldxr_i.c
@@ -0,0 +1,31 @@
+#include "test.h"
+
+static uint32_t data[] = { 0xffffffff, 0x00000000, 0x42424242 };
+
+static void
+run_test(jit_state_t *j, uint8_t *arena_base, size_t arena_size)
+{
+  jit_begin(j, arena_base, arena_size);
+
+  const jit_arg_abi_t abi[] = { JIT_ARG_ABI_POINTER, JIT_ARG_ABI_INTMAX };
+  jit_arg_t args[2];
+  const jit_anyreg_t regs[] = { { .gpr=JIT_R0 }, { .gpr=JIT_R1 } };
+
+  jit_receive(j, 2, abi, args);
+  jit_load_args(j, 2, abi, args, regs);
+
+  jit_ldxr_i(j, JIT_R0, JIT_R0, JIT_R1);
+  jit_retr(j, JIT_R0);
+
+  uintmax_t (*f)(void*, uintmax_t) = jit_end(j, NULL);
+
+  ASSERT(f(data, 0) == -1);
+  ASSERT(f(data, 4) == 0);
+  ASSERT(f(data, 8) == 0x42424242);
+}
+
+int
+main (int argc, char *argv[])
+{
+  return main_helper(argc, argv, run_test);
+}
