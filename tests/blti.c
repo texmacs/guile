329fc7936c0adb69622a2206f8f12f3599a8c2aa
Andy Wingo <wingo@igalia.com>
2019-03-26 14:51:28 +0100

    Add int/int reg/imm compare-and-branch tests

---
+++ tests/blti.c
@@ -0,0 +1,31 @@
+#include "test.h"
+
+static void
+run_test(jit_state_t *j, uint8_t *arena_base, size_t arena_size)
+{
+  jit_begin(j, arena_base, arena_size);
+
+  const jit_arg_abi_t abi[] = { JIT_ARG_ABI_INTMAX };
+  jit_arg_t args[1];
+  const jit_anyreg_t regs[] = { { .gpr=JIT_R0 } };
+
+  jit_receive(j, 1, abi, args);
+  jit_load_args(j, 1, abi, args, regs);
+
+  jit_reloc_t r = jit_blti(j, JIT_R0, 0);
+  jit_reti(j, 0);
+  jit_patch_here(j, r);
+  jit_reti(j, 1);
+
+  intmax_t (*f)(intmax_t) = jit_end(j, NULL);
+
+  ASSERT(f(0) == 0);
+  ASSERT(f(1) == 0);
+  ASSERT(f(-1) == 1);
+}
+
+int
+main (int argc, char *argv[])
+{
+  return main_helper(argc, argv, run_test);
+}
