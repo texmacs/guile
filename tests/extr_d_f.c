81ba8830a21c0e381f1f1e51e7f244dea676a169
Andy Wingo <wingo@igalia.com>
2019-04-27 22:36:06 +0200

    Fix ABI of arg to extr_d_f test

--- tests/extr_d_f.c
+++ tests/extr_d_f.c
@@ -4,7 +4,7 @@ static void
 run_test(jit_state_t *j, uint8_t *arena_base, size_t arena_size)
 {
   jit_begin(j, arena_base, arena_size);
-  jit_load_args_1(j, jit_operand_fpr (JIT_OPERAND_ABI_FLOAT, JIT_F0));
+  jit_load_args_1(j, jit_operand_fpr (JIT_OPERAND_ABI_DOUBLE, JIT_F0));
 
   jit_extr_d_f(j, JIT_F0, JIT_F0);
   jit_retr_f(j, JIT_F0);
