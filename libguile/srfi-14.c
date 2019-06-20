d59dc5fbf2b36d841db35b8d3816d4da92a568a2
Ludovic Courtès <ludo@gnu.org>
2019-06-20 11:34:46 +0200

    srfi-14: Fix possible buffer overrun in '%char-set-dump'.
    * libguile/srfi-14.c (scm_sys_char_set_dump): Change array size from 9
    to 11 as suggested by GCC 9.1.0.

--- libguile/srfi-14.c
+++ libguile/srfi-14.c
@@ -2038,7 +2038,7 @@ SCM_DEFINE (scm_sys_char_set_dump, "%char-set-dump", 1, 0, 0, (SCM charset),
   SCM ranges = SCM_EOL, elt;
   size_t i;
   scm_t_char_set *cs;
-  char codepoint_string_lo[9], codepoint_string_hi[9];
+  char codepoint_string_lo[11], codepoint_string_hi[11];
 
   SCM_VALIDATE_SMOB (1, charset, charset);
   cs = SCM_CHARSET_DATA (charset);
