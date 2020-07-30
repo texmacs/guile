f77d45b6939a958ff801690253f95c181d31baa3
PikachuHy <PikachuHy@163.com>
2020-07-30 12:29:15 +0800

    apply patch
    https://github.com/slowphil/mingw-w64-guile1.8/blob/master/dirent2.patch

--- libguile/filesys.c
+++ libguile/filesys.c
@@ -113,7 +113,24 @@ void *alloca (size_t);
 #include <pwd.h>
 #endif
 
-
+#ifdef __MINGW64__
+#if HAVE_DIRENT_H
+# include <dirent.h>
+# define NAMLEN(dirent) strlen((dirent)->d_name)
+#else
+# define dirent direct
+# define NAMLEN(dirent) (dirent)->d_namlen
+# if HAVE_SYS_NDIR_H
+#  include <sys/ndir.h>
+# endif
+# if HAVE_SYS_DIR_H
+#  include <sys/dir.h>
+# endif
+# if HAVE_NDIR_H
+#  include <ndir.h>
+# endif
+#endif
+#else
 #if defined (__MINGW32__) || defined (_MSC_VER) || defined (__BORLANDC__)
 # include "win32-dirent.h"
 # define NAMLEN(dirent) strlen((dirent)->d_name)
@@ -133,6 +150,7 @@ void *alloca (size_t);
 # if HAVE_NDIR_H
 #  include <ndir.h>
 # endif
+#endif
 #endif
 
 /* Ultrix has S_IFSOCK, but no S_ISSOCK.  Ipe!  */
