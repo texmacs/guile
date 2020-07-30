11422ce6319b529a4a4d0c1f03301a705b8bae28
PikachuHy <PikachuHy@163.com>
2020-07-30 17:53:02 +0800

    check file fun and type

--- config.h.cmake
+++ config.h.cmake
@@ -1,1 +1,93 @@
+
+/* Define this to control the default warning level for deprecated features.
+*/
+#define SCM_WARN_DEPRECATED_DEFAULT "summary"
+
+/* The size of `char', as computed by sizeof. */
+#define SIZEOF_CHAR @SIZEOF_CHAR@
+
+/* The size of `float', as computed by sizeof. */
+#define SIZEOF_FLOAT @SIZEOF_FLOAT@
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT @SIZEOF_INT@
+
+/* The size of `intmax_t', as computed by sizeof. */
+#define SIZEOF_INTMAX_T @SIZEOF_INTMAX_T@
+
+/* The size of `intptr_t', as computed by sizeof. */
+#define SIZEOF_INTPTR_T @SIZEOF_INTPTR_T@
+
+/* The size of `long', as computed by sizeof. */
+#define SIZEOF_LONG @SIZEOF_LONG@
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG @SIZEOF_LONG_LONG@
+
+/* The size of `off_t', as computed by sizeof. */
 #define SIZEOF_OFF_T @SIZEOF_OFF_T@
+
+/* The size of `ptrdiff_t', as computed by sizeof. */
+#define SIZEOF_PTRDIFF_T @SIZEOF_PTRDIFF_T@
+
+/* The size of `short', as computed by sizeof. */
+#define SIZEOF_SHORT @SIZEOF_SHORT@
+
+/* The size of `size_t', as computed by sizeof. */
+#define SIZEOF_SIZE_T @SIZEOF_SIZE_T@
+
+/* The size of `uintptr_t', as computed by sizeof. */
+#define SIZEOF_UINTPTR_T @SIZEOF_UINTPTR_T@
+
+/* The size of `unsigned char', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_CHAR @SIZEOF_UNSIGNED_CHAR@
+
+/* The size of `unsigned int', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_INT @SIZEOF_UNSIGNED_INT@
+
+/* The size of `unsigned long', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG @SIZEOF_UNSIGNED_LONG@
+
+/* The size of `unsigned long long', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG_LONG @SIZEOF_UNSIGNED_LONG_LONG@
+
+/* The size of `unsigned short', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_SHORT @SIZEOF_UNSIGNED_SHORT@
+
+/* The size of `unsigned __int64', as computed by sizeof. */
+#define SIZEOF_UNSIGNED___INT64 @SIZEOF_UNSIGNED___INT64@
+
+/* The size of `void *', as computed by sizeof. */
+#define SIZEOF_VOID_P @SIZEOF_VOID_P@
+
+/* The size of `__int64', as computed by sizeof. */
+#define SIZEOF___INT64 @SIZEOF___INT64@
+
+#define HAVE_DIRENT_H 1
+
+#cmakedefine HAVE_UTIME_H 1
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#cmakedefine HAVE_STRNCASECMP 1
+
+/* Define to 1 if you have the `strptime' function. */
+#cmakedefine HAVE_STRPTIME 1
+
+
+/* Define to 1 if you have the `asinh' function. */
+#cmakedefine HAVE_ASINH 1
+
+/* Define to 1 if you have the <assert.h> header file. */
+#cmakedefine HAVE_ASSERT_H 1
+
+/* Define to 1 if you have the `atanh' function. */
+#cmakedefine HAVE_ATANH 1
+
+/* Define to 1 if you have the `atexit' function. */
+#cmakedefine HAVE_ATEXIT 1
+
+/* Define to 1 if you have the `bcopy' function. */
+#cmakedefine HAVE_BCOPY 1
+
+/* Define to 1 if you have the `cexp' function. */
+#cmakedefine HAVE_CEXP 1
