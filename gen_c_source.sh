b49b22af28c124f07b1d20bd164fa81aadad1940
PikachuHy <pikachuhy@163.com>
2020-08-04 09:52:05 +0800

    fix: fatal error: libguile/cpp_err_symbols.c: No such file or directory

---
+++ gen_c_source.sh
@@ -0,0 +1,8 @@
+files="cpp_err_symbols.in"
+#awk -f cpp_cnvt.awk < cpp_err_symbols.in
+for loop in ${files}
+do
+  file=${loop%.*}
+  echo "generate ${file}.c"
+  awk -f ./libguile/cpp_cnvt.awk < ./libguile/${file}.in > ./cmake-build-debug/libguile/${file}.c
+done
