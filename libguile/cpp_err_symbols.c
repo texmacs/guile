0479605a8c990842b8c9ce4465fed67c75f8da50
Darcy Shen <sadhen@zoho.com>
2020-08-13 21:46:02 +0800

    Add missing

---
+++ libguile/cpp_err_symbols.c
@@ -0,0 +1,366 @@
+#ifdef E2BIG
+scm_c_define ("E2BIG", SCM_MAKINUM (E2BIG));
+#endif
+#ifdef EACCES
+scm_c_define ("EACCES", SCM_MAKINUM (EACCES));
+#endif
+#ifdef EADDRINUSE
+scm_c_define ("EADDRINUSE", SCM_MAKINUM (EADDRINUSE));
+#endif
+#ifdef EADDRNOTAVAIL
+scm_c_define ("EADDRNOTAVAIL", SCM_MAKINUM (EADDRNOTAVAIL));
+#endif
+#ifdef EADV
+scm_c_define ("EADV", SCM_MAKINUM (EADV));
+#endif
+#ifdef EAFNOSUPPORT
+scm_c_define ("EAFNOSUPPORT", SCM_MAKINUM (EAFNOSUPPORT));
+#endif
+#ifdef EAGAIN
+scm_c_define ("EAGAIN", SCM_MAKINUM (EAGAIN));
+#endif
+#ifdef EALREADY
+scm_c_define ("EALREADY", SCM_MAKINUM (EALREADY));
+#endif
+#ifdef EBADE
+scm_c_define ("EBADE", SCM_MAKINUM (EBADE));
+#endif
+#ifdef EBADF
+scm_c_define ("EBADF", SCM_MAKINUM (EBADF));
+#endif
+#ifdef EBADFD
+scm_c_define ("EBADFD", SCM_MAKINUM (EBADFD));
+#endif
+#ifdef EBADMSG
+scm_c_define ("EBADMSG", SCM_MAKINUM (EBADMSG));
+#endif
+#ifdef EBADR
+scm_c_define ("EBADR", SCM_MAKINUM (EBADR));
+#endif
+#ifdef EBADRQC
+scm_c_define ("EBADRQC", SCM_MAKINUM (EBADRQC));
+#endif
+#ifdef EBADSLT
+scm_c_define ("EBADSLT", SCM_MAKINUM (EBADSLT));
+#endif
+#ifdef EBFONT
+scm_c_define ("EBFONT", SCM_MAKINUM (EBFONT));
+#endif
+#ifdef EBUSY
+scm_c_define ("EBUSY", SCM_MAKINUM (EBUSY));
+#endif
+#ifdef ECHILD
+scm_c_define ("ECHILD", SCM_MAKINUM (ECHILD));
+#endif
+#ifdef ECHRNG
+scm_c_define ("ECHRNG", SCM_MAKINUM (ECHRNG));
+#endif
+#ifdef ECOMM
+scm_c_define ("ECOMM", SCM_MAKINUM (ECOMM));
+#endif
+#ifdef ECONNABORTED
+scm_c_define ("ECONNABORTED", SCM_MAKINUM (ECONNABORTED));
+#endif
+#ifdef ECONNREFUSED
+scm_c_define ("ECONNREFUSED", SCM_MAKINUM (ECONNREFUSED));
+#endif
+#ifdef ECONNRESET
+scm_c_define ("ECONNRESET", SCM_MAKINUM (ECONNRESET));
+#endif
+#ifdef EDEADLK
+scm_c_define ("EDEADLK", SCM_MAKINUM (EDEADLK));
+#endif
+#ifdef EDEADLOCK
+scm_c_define ("EDEADLOCK", SCM_MAKINUM (EDEADLOCK));
+#endif
+#ifdef EDESTADDRREQ
+scm_c_define ("EDESTADDRREQ", SCM_MAKINUM (EDESTADDRREQ));
+#endif
+#ifdef EDOM
+scm_c_define ("EDOM", SCM_MAKINUM (EDOM));
+#endif
+#ifdef EDOTDOT
+scm_c_define ("EDOTDOT", SCM_MAKINUM (EDOTDOT));
+#endif
+#ifdef EDQUOT
+scm_c_define ("EDQUOT", SCM_MAKINUM (EDQUOT));
+#endif
+#ifdef EEXIST
+scm_c_define ("EEXIST", SCM_MAKINUM (EEXIST));
+#endif
+#ifdef EFAULT
+scm_c_define ("EFAULT", SCM_MAKINUM (EFAULT));
+#endif
+#ifdef EFBIG
+scm_c_define ("EFBIG", SCM_MAKINUM (EFBIG));
+#endif
+#ifdef EHOSTDOWN
+scm_c_define ("EHOSTDOWN", SCM_MAKINUM (EHOSTDOWN));
+#endif
+#ifdef EHOSTUNREACH
+scm_c_define ("EHOSTUNREACH", SCM_MAKINUM (EHOSTUNREACH));
+#endif
+#ifdef EIDRM
+scm_c_define ("EIDRM", SCM_MAKINUM (EIDRM));
+#endif
+#ifdef EILSEQ
+scm_c_define ("EILSEQ", SCM_MAKINUM (EILSEQ));
+#endif
+#ifdef EINPROGRESS
+scm_c_define ("EINPROGRESS", SCM_MAKINUM (EINPROGRESS));
+#endif
+#ifdef EINTR
+scm_c_define ("EINTR", SCM_MAKINUM (EINTR));
+#endif
+#ifdef EINVAL
+scm_c_define ("EINVAL", SCM_MAKINUM (EINVAL));
+#endif
+#ifdef EIO
+scm_c_define ("EIO", SCM_MAKINUM (EIO));
+#endif
+#ifdef EISCONN
+scm_c_define ("EISCONN", SCM_MAKINUM (EISCONN));
+#endif
+#ifdef EISDIR
+scm_c_define ("EISDIR", SCM_MAKINUM (EISDIR));
+#endif
+#ifdef EISNAM
+scm_c_define ("EISNAM", SCM_MAKINUM (EISNAM));
+#endif
+#ifdef EL2HLT
+scm_c_define ("EL2HLT", SCM_MAKINUM (EL2HLT));
+#endif
+#ifdef EL2NSYNC
+scm_c_define ("EL2NSYNC", SCM_MAKINUM (EL2NSYNC));
+#endif
+#ifdef EL3HLT
+scm_c_define ("EL3HLT", SCM_MAKINUM (EL3HLT));
+#endif
+#ifdef EL3RST
+scm_c_define ("EL3RST", SCM_MAKINUM (EL3RST));
+#endif
+#ifdef ELIBACC
+scm_c_define ("ELIBACC", SCM_MAKINUM (ELIBACC));
+#endif
+#ifdef ELIBBAD
+scm_c_define ("ELIBBAD", SCM_MAKINUM (ELIBBAD));
+#endif
+#ifdef ELIBEXEC
+scm_c_define ("ELIBEXEC", SCM_MAKINUM (ELIBEXEC));
+#endif
+#ifdef ELIBMAX
+scm_c_define ("ELIBMAX", SCM_MAKINUM (ELIBMAX));
+#endif
+#ifdef ELIBSCN
+scm_c_define ("ELIBSCN", SCM_MAKINUM (ELIBSCN));
+#endif
+#ifdef ELNRNG
+scm_c_define ("ELNRNG", SCM_MAKINUM (ELNRNG));
+#endif
+#ifdef ELOOP
+scm_c_define ("ELOOP", SCM_MAKINUM (ELOOP));
+#endif
+#ifdef EMFILE
+scm_c_define ("EMFILE", SCM_MAKINUM (EMFILE));
+#endif
+#ifdef EMLINK
+scm_c_define ("EMLINK", SCM_MAKINUM (EMLINK));
+#endif
+#ifdef EMSGSIZE
+scm_c_define ("EMSGSIZE", SCM_MAKINUM (EMSGSIZE));
+#endif
+#ifdef EMULTIHOP
+scm_c_define ("EMULTIHOP", SCM_MAKINUM (EMULTIHOP));
+#endif
+#ifdef ENAMETOOLONG
+scm_c_define ("ENAMETOOLONG", SCM_MAKINUM (ENAMETOOLONG));
+#endif
+#ifdef ENAVAIL
+scm_c_define ("ENAVAIL", SCM_MAKINUM (ENAVAIL));
+#endif
+#ifdef ENETDOWN
+scm_c_define ("ENETDOWN", SCM_MAKINUM (ENETDOWN));
+#endif
+#ifdef ENETRESET
+scm_c_define ("ENETRESET", SCM_MAKINUM (ENETRESET));
+#endif
+#ifdef ENETUNREACH
+scm_c_define ("ENETUNREACH", SCM_MAKINUM (ENETUNREACH));
+#endif
+#ifdef ENFILE
+scm_c_define ("ENFILE", SCM_MAKINUM (ENFILE));
+#endif
+#ifdef ENOANO
+scm_c_define ("ENOANO", SCM_MAKINUM (ENOANO));
+#endif
+#ifdef ENOBUFS
+scm_c_define ("ENOBUFS", SCM_MAKINUM (ENOBUFS));
+#endif
+#ifdef ENOCSI
+scm_c_define ("ENOCSI", SCM_MAKINUM (ENOCSI));
+#endif
+#ifdef ENODATA
+scm_c_define ("ENODATA", SCM_MAKINUM (ENODATA));
+#endif
+#ifdef ENODEV
+scm_c_define ("ENODEV", SCM_MAKINUM (ENODEV));
+#endif
+#ifdef ENOENT
+scm_c_define ("ENOENT", SCM_MAKINUM (ENOENT));
+#endif
+#ifdef ENOEXEC
+scm_c_define ("ENOEXEC", SCM_MAKINUM (ENOEXEC));
+#endif
+#ifdef ENOLCK
+scm_c_define ("ENOLCK", SCM_MAKINUM (ENOLCK));
+#endif
+#ifdef ENOLINK
+scm_c_define ("ENOLINK", SCM_MAKINUM (ENOLINK));
+#endif
+#ifdef ENOMEM
+scm_c_define ("ENOMEM", SCM_MAKINUM (ENOMEM));
+#endif
+#ifdef ENOMSG
+scm_c_define ("ENOMSG", SCM_MAKINUM (ENOMSG));
+#endif
+#ifdef ENONET
+scm_c_define ("ENONET", SCM_MAKINUM (ENONET));
+#endif
+#ifdef ENOPKG
+scm_c_define ("ENOPKG", SCM_MAKINUM (ENOPKG));
+#endif
+#ifdef ENOPROTOOPT
+scm_c_define ("ENOPROTOOPT", SCM_MAKINUM (ENOPROTOOPT));
+#endif
+#ifdef ENOSPC
+scm_c_define ("ENOSPC", SCM_MAKINUM (ENOSPC));
+#endif
+#ifdef ENOSR
+scm_c_define ("ENOSR", SCM_MAKINUM (ENOSR));
+#endif
+#ifdef ENOSTR
+scm_c_define ("ENOSTR", SCM_MAKINUM (ENOSTR));
+#endif
+#ifdef ENOSYS
+scm_c_define ("ENOSYS", SCM_MAKINUM (ENOSYS));
+#endif
+#ifdef ENOTBLK
+scm_c_define ("ENOTBLK", SCM_MAKINUM (ENOTBLK));
+#endif
+#ifdef ENOTCONN
+scm_c_define ("ENOTCONN", SCM_MAKINUM (ENOTCONN));
+#endif
+#ifdef ENOTDIR
+scm_c_define ("ENOTDIR", SCM_MAKINUM (ENOTDIR));
+#endif
+#ifdef ENOTEMPTY
+scm_c_define ("ENOTEMPTY", SCM_MAKINUM (ENOTEMPTY));
+#endif
+#ifdef ENOTNAM
+scm_c_define ("ENOTNAM", SCM_MAKINUM (ENOTNAM));
+#endif
+#ifdef ENOTSOCK
+scm_c_define ("ENOTSOCK", SCM_MAKINUM (ENOTSOCK));
+#endif
+#ifdef ENOTTY
+scm_c_define ("ENOTTY", SCM_MAKINUM (ENOTTY));
+#endif
+#ifdef ENOTUNIQ
+scm_c_define ("ENOTUNIQ", SCM_MAKINUM (ENOTUNIQ));
+#endif
+#ifdef ENXIO
+scm_c_define ("ENXIO", SCM_MAKINUM (ENXIO));
+#endif
+#ifdef EOPNOTSUPP
+scm_c_define ("EOPNOTSUPP", SCM_MAKINUM (EOPNOTSUPP));
+#endif
+#ifdef EOVERFLOW
+scm_c_define ("EOVERFLOW", SCM_MAKINUM (EOVERFLOW));
+#endif
+#ifdef EPERM
+scm_c_define ("EPERM", SCM_MAKINUM (EPERM));
+#endif
+#ifdef EPFNOSUPPORT
+scm_c_define ("EPFNOSUPPORT", SCM_MAKINUM (EPFNOSUPPORT));
+#endif
+#ifdef EPIPE
+scm_c_define ("EPIPE", SCM_MAKINUM (EPIPE));
+#endif
+#ifdef EPROTO
+scm_c_define ("EPROTO", SCM_MAKINUM (EPROTO));
+#endif
+#ifdef EPROTONOSUPPORT
+scm_c_define ("EPROTONOSUPPORT", SCM_MAKINUM (EPROTONOSUPPORT));
+#endif
+#ifdef EPROTOTYPE
+scm_c_define ("EPROTOTYPE", SCM_MAKINUM (EPROTOTYPE));
+#endif
+#ifdef ERANGE
+scm_c_define ("ERANGE", SCM_MAKINUM (ERANGE));
+#endif
+#ifdef EREMCHG
+scm_c_define ("EREMCHG", SCM_MAKINUM (EREMCHG));
+#endif
+#ifdef EREMOTE
+scm_c_define ("EREMOTE", SCM_MAKINUM (EREMOTE));
+#endif
+#ifdef EREMOTEIO
+scm_c_define ("EREMOTEIO", SCM_MAKINUM (EREMOTEIO));
+#endif
+#ifdef ERESTART
+scm_c_define ("ERESTART", SCM_MAKINUM (ERESTART));
+#endif
+#ifdef EROFS
+scm_c_define ("EROFS", SCM_MAKINUM (EROFS));
+#endif
+#ifdef ESHUTDOWN
+scm_c_define ("ESHUTDOWN", SCM_MAKINUM (ESHUTDOWN));
+#endif
+#ifdef ESOCKTNOSUPPORT
+scm_c_define ("ESOCKTNOSUPPORT", SCM_MAKINUM (ESOCKTNOSUPPORT));
+#endif
+#ifdef ESPIPE
+scm_c_define ("ESPIPE", SCM_MAKINUM (ESPIPE));
+#endif
+#ifdef ESRCH
+scm_c_define ("ESRCH", SCM_MAKINUM (ESRCH));
+#endif
+#ifdef ESRMNT
+scm_c_define ("ESRMNT", SCM_MAKINUM (ESRMNT));
+#endif
+#ifdef ESTALE
+scm_c_define ("ESTALE", SCM_MAKINUM (ESTALE));
+#endif
+#ifdef ESTRPIPE
+scm_c_define ("ESTRPIPE", SCM_MAKINUM (ESTRPIPE));
+#endif
+#ifdef ETIME
+scm_c_define ("ETIME", SCM_MAKINUM (ETIME));
+#endif
+#ifdef ETIMEDOUT
+scm_c_define ("ETIMEDOUT", SCM_MAKINUM (ETIMEDOUT));
+#endif
+#ifdef ETOOMANYREFS
+scm_c_define ("ETOOMANYREFS", SCM_MAKINUM (ETOOMANYREFS));
+#endif
+#ifdef ETXTBSY
+scm_c_define ("ETXTBSY", SCM_MAKINUM (ETXTBSY));
+#endif
+#ifdef EUCLEAN
+scm_c_define ("EUCLEAN", SCM_MAKINUM (EUCLEAN));
+#endif
+#ifdef EUNATCH
+scm_c_define ("EUNATCH", SCM_MAKINUM (EUNATCH));
+#endif
+#ifdef EUSERS
+scm_c_define ("EUSERS", SCM_MAKINUM (EUSERS));
+#endif
+#ifdef EWOULDBLOCK
+scm_c_define ("EWOULDBLOCK", SCM_MAKINUM (EWOULDBLOCK));
+#endif
+#ifdef EXDEV
+scm_c_define ("EXDEV", SCM_MAKINUM (EXDEV));
+#endif
+#ifdef EXFULL
+scm_c_define ("EXFULL", SCM_MAKINUM (EXFULL));
+#endif
